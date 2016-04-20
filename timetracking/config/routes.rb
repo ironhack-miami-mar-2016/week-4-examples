0fe9a51207003d7c67274437d4e85aba94b381a6
Nizar Khalife Iglesias <khalifenizar@gmail.com>
2016-04-20 09:04:11 -0400

    Time Tracking: Add projects index

--- timetracking/config/routes.rb
+++ timetracking/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get '/', to: 'site#home'
 
+  get '/projects', to: 'projects#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
