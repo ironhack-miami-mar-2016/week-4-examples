9360dfa3c8af9fbe3437966e65d5a198e694088f
Nizar Khalife Iglesias <khalifenizar@gmail.com>
2016-04-21 11:28:21 -0400

    Time Tracking: Add clean_old method

--- timetracking/app/models/project.rb
+++ timetracking/app/models/project.rb
@@ -1,2 +1,15 @@
 class Project < ActiveRecord::Base
+  has_many :time_entries
+
+  def self.clean_old
+    old_projects = Project.where("created_at < ?", 1.week.ago)
+
+    # Also for a week ago:
+    # 7.days.ago
+    # Time.now - 60 * 60 * 24 * 7
+    # Time.now - 7.days
+    # Time.now - 1.week
+
+    old_projects.destroy_all
+  end
 end
